a024396f4223a4b4219bc0784e0e91ae2b42ae0c
masashi-yamashita <47632934+guuma@users.noreply.github.com>
2022-10-28 16:45:04 +0900

    initialize spring-data-jpa-practice

---
+++ spring-data-jpa-practice/src/main/java/spring/data/jpa/practice/springdatajpapractice/SpringDataJpaPracticeApplication.java
@@ -0,0 +1,13 @@
+package spring.data.jpa.practice.springdatajpapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaPracticeApplication.class, args);
+	}
+
+}
---
+++ spring-data-jpa-practice/src/test/java/spring/data/jpa/practice/springdatajpapractice/SpringDataJpaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.data.jpa.practice.springdatajpapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
